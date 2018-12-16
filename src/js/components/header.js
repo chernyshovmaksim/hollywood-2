9dbf24746401203e236afcdd4f4bddac20f120a8
Max Chernyshov <chernyshovmaksim56@gmail.com>
2018-12-16 11:41:53 +0500

    add background image to first screen

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,8 +1,8 @@
 export default class Header {
   static headerFullScreen() {
-    const headerH = $("header").height();
-    const topHeaderH = $(".top-header").height();
-		const menuHeaderH = $(".menu-header").height();
+    const headerH = $("header").outerHeight();
+    const topHeaderH = $(".top-header").outerHeight();
+		const menuHeaderH = $(".menu-header").outerHeight();
 		const imageHeaderH = headerH - topHeaderH - menuHeaderH;
 		const imageHeader = $(".image-header").height(`${imageHeaderH}px`);
   }
